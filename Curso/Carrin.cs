ae22a37f98f2986a34e808a4444fe58954d74c85
Atauine Dias <atauine@roko.com.br>
2024-03-07 22:05:32 -0300

    Primeiro Commit

---
+++ Curso/Carrin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso
+{
+   class Carrin
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bom Dia");
+
+        }
+    }
+}
---
+++ Curso/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso
+{
+    internal class Produto
+    {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+
+        public double ValorTotalEmEstoque() { 
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $"
+                + Preco.ToString("F2")
+                + ", "
+                + Quantidade
+                + " unidades, Total $"
+                + ValorTotalEmEstoque().ToString("F2") ;
+        }
+    }
+}
